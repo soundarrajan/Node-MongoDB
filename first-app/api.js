ae587fefafe69637f63df2d51552399336e2905f
Soundar <soundarrajan.s@inatech.com>
2022-10-18 19:13:23 +0530

    api response - added

--- first-app/api.js
+++ first-app/api.js
@@ -1,6 +1,48 @@
-if(1==1){
-const response = await fetch("https://jsonplaceholder.typicode.com/users");
+// if(1==1){
+// const response = await fetch("https://jsonplaceholder.typicode.com/users");
 
-const data = await response.json();
-}
-console.log(data);
+// const data = await response.json();
+// }
+// console.log(data);
+
+const express = require('express');
+const https  = require('https');
+const app = express();
+const url = 'https://jsonplaceholder.typicode.com/users';
+app.get('/',(req,res)=>{
+ //res.send("server running");
+//  https.get(url,(response)=>{
+//    //console.log(response1);
+//    console.log('statusCode:', response.statusCode);
+//   //console.log('headers:', res.headers);
+//  })
+
+https.get('https://jsonplaceholder.typicode.com/users', (response) => {
+//   console.log('statusCode:', res.statusCode);
+ //  console.log('headers:', res.headers);
+
+//   res.on('data', (d) => {
+//     //process.stdout.write(d);
+//    const datas = JSON.parse(d);
+//    console.log(datas);
+//   });
+var newsItems = '';
+response.on("data", function (data) {
+    newsItems += data;
+});
+
+response.on("end", function () {
+    var jsonParse = JSON.parse(newsItems);
+    console.log(jsonParse);
+});
+
+}).on('error', (e) => {
+  console.error(e);
+});
+ 
+ 
+ console.log("started");
+ res.send("response");
+})
+
+app.listen(3000);
