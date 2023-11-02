91ff711686d97374907c2b6c04a298f75b357ba0
soundarrajan <mailforsoundar@gmail.com>
2023-11-02 21:52:55 +0530

    mongo

--- mongo-node/app.js
+++ mongo-node/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { ObjectId } = require('mongodb');
 const { connectToDB, getDB }  = require('./db.js');
 const app = express();
 
 // check db connection
+app.use(express.json())
 let db;
 
 connectToDB((err) => {
@@ -34,3 +36,70 @@ app.get('/',(req,res) => {
     res.send('its running');
 })
 
+app.get('/bookd/:id',(req,res) => {
+
+   if(ObjectId.isValid(req.params.id)){
+    db.collection('books')
+    .findOne({_id:ObjectId(req.params.id)})
+    .then((doc) =>{
+        res.status(200).json(doc);
+    })
+    .catch(() => {
+        res.status(500).json({error:'error'})
+    })
+
+    res.send('its running');
+} else{
+    res.status(500).json({error:'error'})
+}
+})
+
+app.post('/books',(req,res) => {
+const book= req.body;
+    db.collection('books')
+    .insertOne(book)
+    .then((result) =>{
+        res.status(200).json(result);
+    })
+    .catch(() => {
+        res.status(500).json({error:'error'})
+    })
+
+    res.send('its running');
+})
+
+app.delete('/bookd/:id',(req,res) => {
+
+    if(ObjectId.isValid(req.params.id)){
+     db.collection('books')
+     .deleteOne({_id:ObjectId(req.params.id)})
+     .then((doc) =>{
+         res.status(200).json(doc);
+     })
+     .catch(() => {
+         res.status(500).json({error:'error'})
+     })
+ 
+     res.send('its running');
+ } else{
+     res.status(500).json({error:'error'})
+ }
+ })
+
+ app.patch('/books/:id',(req,res) => {
+    const update= req.body;
+    if(ObjectId.isValid(req.params.id)){
+        db.collection('books')
+        .updateOne({_id:ObjectId(req.params.id)},{$set:{update}})
+        .then((doc) =>{
+            res.status(200).json(doc);
+        })
+        .catch(() => {
+            res.status(500).json({error:'error'})
+        })
+    
+        res.send('its running');
+    } else{
+        res.status(500).json({error:'error'})
+    }
+ })
